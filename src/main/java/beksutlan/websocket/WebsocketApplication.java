f66c89fb34f460cb16bae7aea78edcdec5817b79
BeksultanDev <beksultancs@gmail.com>
2023-03-03 10:18:25 +0600

    hello websocket

---
+++ src/main/java/beksutlan/websocket/WebsocketApplication.java
@@ -0,0 +1,92 @@
+package beksutlan.websocket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@SpringBootApplication
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+
+}
+
+@Configuration
+@EnableWebSocket
+class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new MyWebSocketHander(), "/websocket").setAllowedOrigins("*");
+    }
+}
+
+@Slf4j
+class MyWebSocketHander implements WebSocketHandler {
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("session.getHandshakeHeaders().get(\"name\") = " + session.getHandshakeHeaders().get("name"));
+        log.info("New User Connected {}", session.getId());
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        var payload = (String) message.getPayload();
+        MyMessage myMessage = new ObjectMapper().readValue(payload, MyMessage.class);
+        log.info("Received from '{}' message '{}'", myMessage.getName(), myMessage.getMessage());
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+}
+
+class MyMessage {
+    private String name;
+    private String message;
+
+    public MyMessage() {
+    }
+
+    public MyMessage(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
